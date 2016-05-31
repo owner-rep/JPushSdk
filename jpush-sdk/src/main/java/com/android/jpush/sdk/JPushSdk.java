734fb6a1ea578dcbea70242badef2b09777063d1
uddream <wangweiguo@lifang.com>
2016-05-31 18:42:06 +0800

    a

--- jpush-sdk/src/main/java/com/android/jpush/sdk/JPushSdk.java
+++ jpush-sdk/src/main/java/com/android/jpush/sdk/JPushSdk.java
@@ -1,5 +1,6 @@
 package com.android.jpush.sdk;
 
+import android.app.Activity;
 import android.content.Context;
 
 import cn.jpush.android.api.JPushInterface;
@@ -8,9 +9,18 @@ import cn.jpush.android.api.JPushInterface;
  * Created by Glen on 2016/5/17.
  */
 public class JPushSdk {
+    public static Class<? extends Activity> sHandleActivity;
 
     public static void init(Context context, boolean debug) {
         JPushInterface.setDebugMode(debug);
         JPushInterface.init(context);
+    }
+
+    public static Class<? extends Activity> getHandleActivity() {
+        return sHandleActivity;
+    }
+
+    public static void setHandleActivity(Class<? extends Activity> sHandleActivity) {
+        JPushSdk.sHandleActivity = sHandleActivity;
     }
 }
